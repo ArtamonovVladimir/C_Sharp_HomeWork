2b4c5e46ef8435024feb37c44835029943cacf65
Vladimir_Artamonov <tomodachiva@gmail.com>
2022-06-25 11:45:21 +0300

    Task 2 was done and code is in 2_TASK_1_HW Folder and 2_Task.cs file

---
+++ 2_TASK_1_HW/2_Task.cs
@@ -0,0 +1,21 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите сколько будет всего чисел, для определения из них максимального и минимального: ");
+int lastNumber = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[lastNumber];
+int index = 0;
+int lengthOfArray = array.Length;
+Console.Write($"Введите {index + 1} целое число: ");
+array[index] = Convert.ToDouble(Console.ReadLine());
+double maxNumber = array[index];
+double minNumber = array[index];
+index++;
+while (index < lengthOfArray)
+{
+    Console.Write($"Введите {index + 1} целое число: ");
+    array[index] = Convert.ToDouble(Console.ReadLine());
+    if (maxNumber < array[index]) maxNumber = array[index];
+    if (minNumber > array[index]) minNumber = array[index];
+    index++;
+}
+Console.WriteLine($"Максимальное число в Вашем ряду составляет {maxNumber}");
+Console.Write($"Минимальное число в Вашем ряду составляет {minNumber}");
