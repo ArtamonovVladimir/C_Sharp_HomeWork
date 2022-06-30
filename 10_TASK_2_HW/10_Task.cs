416b10e6d85887ecabce4bd0874e4f687b6363fa
Vladimir_Artamonov <tomodachiva@gmail.com>
2022-06-30 14:36:45 +0300

    Task 10 was done and code is in 10_TASK_2_HW Folder and 10_Task.cs file

---
+++ 10_TASK_2_HW/10_Task.cs
@@ -0,0 +1,20 @@
+﻿namespace VladimirArtamnov
+ {
+    class HomeWork_Task_10
+    {
+        public static int GetRandomNumber(int first, int second)
+        {
+            int randomNumber = new Random().Next(first, second);
+            return randomNumber;
+        }
+        
+        public static void Main(string[] args)
+        {
+            int number = GetRandomNumber(100,1000);
+            string array = Convert.ToString(number);
+            int secondChar = Convert.ToInt32(array[1].ToString());
+            Console.WriteLine($"Вторым числом в трехзначном {number} является {secondChar}");
+
+        }
+    }
+ }
