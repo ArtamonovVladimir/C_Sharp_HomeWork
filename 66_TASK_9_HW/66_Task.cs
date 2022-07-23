fcf26de3832668ee0bc21b08d0eb98d13fdbabf2
Vladimir_Artamonov <tomodachiva@gmail.com>
2022-07-23 18:35:45 +0300

    Task 66 was done and code is in 66_TASK_9_HW Folder and 66_Task.cs file

---
+++ 66_TASK_9_HW/66_Task.cs
@@ -0,0 +1,33 @@
+﻿namespace VladimirArtamonov
+{
+    public static class HomeWorkTask66
+    {
+        //Задача 66. Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+        //M = 1; N = 15 -> 120
+        //M = 4; N = 8. -> 30
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значения M и N, между которыми (включая их) будет найдена и напечатана на экране сумма всех натуральных чисел.");
+            Console.Write("\nВведите значения M:\t");
+            int numberM = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nВведите значения N:\t");
+            int numberN = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"\nСумма натуральных чисел в промежутке от {numberM} до {numberN} равно:\t");
+            Console.Write(RecursionSumCulculationOfNaturalNumbers(numberM, numberN));
+        }
+
+
+        public static int RecursionSumCulculationOfNaturalNumbers(int startNumber, int endNumber)
+        {
+            if (endNumber < startNumber)
+            {
+                return 0;
+            }
+            else
+            {
+                return endNumber + RecursionSumCulculationOfNaturalNumbers(startNumber, endNumber - 1);
+            }
+        }
+    }
+}
